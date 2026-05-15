780f7c2a8ce4b9f4eea2df8570259db6ddac3de9
Денис Борозинец <denis.borozinets@yandex.ru>
2026-05-15 16:25:21 +0300

    1. Открытие/закрытие выезжающего меню.

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,14 +2,35 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [asideClassName, setAsideClassName] = useState([styles.container]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen(!isOpen);
+					if (isOpen) {
+						setAsideClassName([styles.container]);
+					} else {
+						setAsideClassName([styles.container, styles.container_open]);
+					}
+				}}
+			/>
+			<aside className={asideClassName.join(' ')}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
+						{/* <Select /> */}
+						{/* <RadioGroup /> */}
+						{/* <Select /> */}
+						<Separator />
+						{/* <Select /> */}
+						{/* <Select /> */}
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
