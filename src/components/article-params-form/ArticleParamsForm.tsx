23dad7cdec09561b991a54c09ad21d0338cfbdd2
Денис Борозинец <denis.borozinets@yandex.ru>
2026-05-15 20:23:43 +0300

    1. тест

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -31,7 +31,6 @@ export const ArticleParamsForm = () => {
 						<Separator />
 						{/* <Select /> */}
 						{/* <Select /> */}
-						{/* <Select /> */}
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
