915bf199b1c4d17a1669d12aaa47cb01ae2d538b
Денис Борозинец <denis.borozinets@yandex.ru>
2026-05-16 14:48:44 +0300

    1. Закрытие сайдбара при клике вне сайдбара. 2. Заполнение списка шрифтов. 3. Сброс шрифта на дефолтное значение.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
@@ -8,8 +8,26 @@ import { defaultArticleState } from './../../constants/articleProps';
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(true);
+
 	return (
 		<main
+			/* Нажатие вне сайдбара. */
+			onClick={(e) => {
+				const target = e.target as HTMLElement;
+				/* Конечно, будь в кнопке id - было бы проще, но что имеем. */
+				const isButton =
+					target.getAttribute('role') === 'button' ||
+					target.parentElement?.getAttribute('role') === 'button';
+
+				if (
+					isOpen &&
+					!isButton &&
+					!document.getElementsByTagName('aside')[0].contains(target)
+				) {
+					setIsOpen(false);
+				}
+			}}
 			className={clsx(styles.main)}
 			style={
 				{
@@ -20,7 +38,7 @@ export const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm isOpen={isOpen} setIsOpen={setIsOpen} />
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,36 +2,100 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+//import { Separator } from 'src/ui/separator';
+import { Select } from 'src/ui/select';
+import { OptionType } from 'src/constants/articleProps';
 import { useState } from 'react';
-import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const [asideClassName, setAsideClassName] = useState([styles.container]);
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const [defaultFontFamily] = useState({
+		title: 'Open Sans',
+		value: 'OpenSans',
+		className: 'string',
+	});
+	const [selectedFontFamily, setSelectedFontFamily] = useState({
+		title: 'Open Sans',
+		value: 'OpenSans',
+		className: 'string',
+	});
+
+	const asideClassName: any[] = [styles.container];
+	if (props.isOpen) {
+		asideClassName.push(styles.container_open);
+	}
+
+	//Наверное в app надо хранить эти списки чтобы при перерендеринге постоянно их не воссоздавать.
+
+	const fontFamilyOptions: OptionType[] = [
+		{
+			title: 'Open Sans',
+			value: 'OpenSans',
+			className: 'string',
+		},
+		{
+			title: 'Ubuntu',
+			value: 'Ubuntu',
+			className: 'string',
+		},
+		{
+			title: 'Cormorant Garamond',
+			value: 'CormorantGaramond',
+			className: 'string',
+		},
+		{
+			title: 'Days One',
+			value: 'DaysOne',
+			className: 'string',
+		},
+		{
+			title: 'Merriweather',
+			value: 'Merriweather',
+			className: 'string',
+		},
+	];
 
 	return (
 		<>
 			<ArrowButton
-				isOpen={isOpen}
+				isOpen={props.isOpen}
 				onClick={() => {
-					setIsOpen(!isOpen);
-					if (isOpen) {
-						setAsideClassName([styles.container]);
-					} else {
-						setAsideClassName([styles.container, styles.container_open]);
-					}
+					props.setIsOpen(!props.isOpen);
 				}}
 			/>
 			<aside className={asideClassName.join(' ')}>
-				<form className={styles.form}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						setSelectedFontFamily(defaultFontFamily);
+					}}>
+					<Select
+						selected={selectedFontFamily}
+						options={fontFamilyOptions}
+						title='шрифт'
+						onChange={(selectedElement) => {
+							setSelectedFontFamily(selectedElement);
+						}}
+					/>
+					{/* <RadioGroup /> 
+					<Select selected={null} options={fontFamilyOptions} title='цвет шрифта' />
+					<Separator />
+					<Select selected={null} options={fontFamilyOptions} title='цвет фона' />
+					<Select selected={null} options={fontFamilyOptions} title='ширина контента' />*/}
 					<div className={styles.bottomContainer}>
-						{/* <Select /> */}
-						{/* <RadioGroup /> */}
-						{/* <Select /> */}
-						<Separator />
-						{/* <Select /> */}
-						{/* <Select /> */}
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								setSelectedFontFamily(defaultFontFamily);
+							}}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
