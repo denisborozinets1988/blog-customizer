bec58506af47b497188e666a9b2a1297716869b5
Денис Борозинец <denis.borozinets@yandex.ru>
2026-05-18 14:32:53 +0300

    1. Реализован весь функционал по заданию. Проект готов к проверке.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,27 @@
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useRef, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	OptionType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
-	const [isOpen, setIsOpen] = useState<boolean>(true);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [, setApply] = useState({});
+	const fontFamilyRef = useRef<OptionType>(
+		defaultArticleState.fontFamilyOption
+	);
+	const fontSizeRef = useRef<OptionType>(defaultArticleState.fontSizeOption);
+	const fontColorRef = useRef<OptionType>(defaultArticleState.fontColor);
+	const backgroundColorRef = useRef<OptionType>(
+		defaultArticleState.backgroundColor
+	);
+	const contentWidthRef = useRef<OptionType>(defaultArticleState.contentWidth);
 
 	return (
 		<main
@@ -31,14 +44,44 @@ export const App = () => {
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': fontFamilyRef.current.value,
+					'--font-size': fontSizeRef.current.value,
+					'--font-color': fontColorRef.current.value,
+					'--container-width': contentWidthRef.current.value,
+					'--bg-color': backgroundColorRef.current.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm isOpen={isOpen} setIsOpen={setIsOpen} />
+			<ArticleParamsForm
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+				fontFamilyRef={fontFamilyRef}
+				fontSizeRef={fontSizeRef}
+				fontColorRef={fontColorRef}
+				backgroundColorRef={backgroundColorRef}
+				contentWidthRef={contentWidthRef}
+				Apply={(
+					fontFamily: OptionType,
+					fontSize: OptionType,
+					fontColor: OptionType,
+					backgroundColor: OptionType,
+					contentWidth: OptionType
+				) => {
+					fontFamilyRef.current = fontFamily;
+					fontSizeRef.current = fontSize;
+					fontColorRef.current = fontColor;
+					backgroundColorRef.current = backgroundColor;
+					contentWidthRef.current = contentWidth;
+					setApply({});
+				}}
+				Reset={() => {
+					fontFamilyRef.current = defaultArticleState.fontFamilyOption;
+					fontSizeRef.current = defaultArticleState.fontSizeOption;
+					fontColorRef.current = defaultArticleState.fontColor;
+					backgroundColorRef.current = defaultArticleState.backgroundColor;
+					contentWidthRef.current = defaultArticleState.contentWidth;
+					setApply({});
+				}}
+			/>
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,62 +2,70 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
-//import { Separator } from 'src/ui/separator';
 import { Select } from 'src/ui/select';
-import { OptionType } from 'src/constants/articleProps';
-import { useState } from 'react';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { useEffect, useState } from 'react';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 type ArticleParamsFormProps = {
 	isOpen: boolean;
 	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	fontFamilyRef: React.MutableRefObject<OptionType>;
+	fontSizeRef: React.MutableRefObject<OptionType>;
+	fontColorRef: React.MutableRefObject<OptionType>;
+	backgroundColorRef: React.MutableRefObject<OptionType>;
+	contentWidthRef: React.MutableRefObject<OptionType>;
+	Apply: (
+		fontFamily: OptionType,
+		fontSize: OptionType,
+		fontColor: OptionType,
+		backgroundColor: OptionType,
+		contentWidth: OptionType
+	) => void;
+	Reset: () => void;
 };
 
 export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
-	const [defaultFontFamily] = useState({
-		title: 'Open Sans',
-		value: 'OpenSans',
-		className: 'string',
-	});
-	const [selectedFontFamily, setSelectedFontFamily] = useState({
-		title: 'Open Sans',
-		value: 'OpenSans',
-		className: 'string',
-	});
-
+	const [selectedFontFamily, setSelectedFontFamily] = useState(
+		props.fontFamilyRef.current
+	);
+	const [selectedFontSize, setSelectedFontSize] = useState(
+		props.fontSizeRef.current
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState(
+		props.fontColorRef.current
+	);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(
+		props.backgroundColorRef.current
+	);
+	const [selectedContentWidth, setSelectedContentWidth] = useState(
+		props.contentWidthRef.current
+	);
 	const asideClassName: any[] = [styles.container];
 	if (props.isOpen) {
 		asideClassName.push(styles.container_open);
 	}
 
-	//Наверное в app надо хранить эти списки чтобы при перерендеринге постоянно их не воссоздавать.
-
-	const fontFamilyOptions: OptionType[] = [
-		{
-			title: 'Open Sans',
-			value: 'OpenSans',
-			className: 'string',
-		},
-		{
-			title: 'Ubuntu',
-			value: 'Ubuntu',
-			className: 'string',
-		},
-		{
-			title: 'Cormorant Garamond',
-			value: 'CormorantGaramond',
-			className: 'string',
-		},
-		{
-			title: 'Days One',
-			value: 'DaysOne',
-			className: 'string',
-		},
-		{
-			title: 'Merriweather',
-			value: 'Merriweather',
-			className: 'string',
-		},
-	];
+	/* Сброс неприменённых настроек на текущие при закрытии. */
+	useEffect(() => {
+		if (!props.isOpen) {
+			setSelectedFontFamily(props.fontFamilyRef.current);
+			setSelectedFontSize(props.fontSizeRef.current);
+			setSelectedFontColor(props.fontColorRef.current);
+			setSelectedBackgroundColor(props.backgroundColorRef.current);
+			setSelectedContentWidth(props.contentWidthRef.current);
+		}
+	}, [props.isOpen]);
 
 	return (
 		<>
@@ -69,11 +77,21 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 			/>
 			<aside className={asideClassName.join(' ')}>
 				<form
+					style={{ gap: 50 }}
 					className={styles.form}
 					onSubmit={(e) => {
 						e.preventDefault();
-						setSelectedFontFamily(defaultFontFamily);
+						props.Apply(
+							selectedFontFamily,
+							selectedFontSize,
+							selectedFontColor,
+							selectedBackgroundColor,
+							selectedContentWidth
+						);
 					}}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						задайте параметры
+					</Text>
 					<Select
 						selected={selectedFontFamily}
 						options={fontFamilyOptions}
@@ -82,18 +100,52 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 							setSelectedFontFamily(selectedElement);
 						}}
 					/>
-					{/* <RadioGroup /> 
-					<Select selected={null} options={fontFamilyOptions} title='цвет шрифта' />
+					<RadioGroup
+						name='размер шрифта'
+						options={fontSizeOptions}
+						selected={selectedFontSize}
+						onChange={(selectedElement) => {
+							setSelectedFontSize(selectedElement);
+						}}
+						title='размер шрифта'
+					/>
+					<Select
+						selected={selectedFontColor}
+						options={fontColors}
+						title='цвет шрифта'
+						onChange={(selectedElement) => {
+							setSelectedFontColor(selectedElement);
+						}}
+					/>
 					<Separator />
-					<Select selected={null} options={fontFamilyOptions} title='цвет фона' />
-					<Select selected={null} options={fontFamilyOptions} title='ширина контента' />*/}
+					<Select
+						selected={selectedBackgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+						onChange={(selectedElement) => {
+							setSelectedBackgroundColor(selectedElement);
+						}}
+					/>
+					<Select
+						selected={selectedContentWidth}
+						options={contentWidthArr}
+						title='ширина контента'
+						onChange={(selectedElement) => {
+							setSelectedContentWidth(selectedElement);
+						}}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button
 							title='Сбросить'
 							htmlType='reset'
 							type='clear'
 							onClick={() => {
-								setSelectedFontFamily(defaultFontFamily);
+								setSelectedFontFamily(defaultArticleState.fontFamilyOption);
+								setSelectedFontSize(defaultArticleState.fontSizeOption);
+								setSelectedFontColor(defaultArticleState.fontColor);
+								setSelectedBackgroundColor(defaultArticleState.backgroundColor);
+								setSelectedContentWidth(defaultArticleState.contentWidth);
+								props.Reset();
 							}}
 						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
